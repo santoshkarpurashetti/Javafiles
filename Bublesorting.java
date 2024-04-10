cb48e373beb9cbf6a5458bc1b9020b7046bdf4b4
Santosh <karpurashettis@gmail.com>
2024-04-10 21:37:36 +0530

    third

--- Bublesorting.java
+++ Bublesorting.java
@@ -14,7 +14,7 @@ class BubbleSort {
     void printArray(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + " ");a
         System.out.println();
     }
 }
